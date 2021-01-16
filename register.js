c91c07d85d7267233ce54dbd856f7311fb1b1a4c
Bryan Beeley <bryan@lifelink.com>
2021-01-15 16:05:02 -0800

    Attempt to switch register to using node-yaml

--- register.js
+++ register.js
@@ -1,12 +1,8 @@
 var fs   = require('fs');
-var yaml = require('js-yaml');
-var includeSchema = require('./includeSchema');
+var yaml = require('node-yaml');
 
 var yamlRequire = function (m, f) {
-  m.exports = yaml.safeLoad(fs.readFileSync(f, 'utf8'), {
-    schema: includeSchema,
-    filename: f
-  });
+  m.exports = yaml.readSync(f);
 };
 
 // Hack to prevent later changes of .yml, .yaml extensions handlers
